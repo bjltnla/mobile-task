e7b0d47afaa20ec9e6a8894ca7abc247e6384835
Rangga <rangaaa.pratamaaa@gmail.com>
2026-02-11 03:24:11 +0700

    fix2

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,5 @@
-import { View, Text, Button } from 'react-native';
-import { useState } from 'react';
+import { router } from 'expo-router';
 
 export default function Home() {
-  const [data, setData] = useState('');
-
-  const getData = async () => {
-    try {
-      const res = await fetch('http://192.168.1.10:8000/api/test');
-      const json = await res.json();
-      setData(json.message);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  return (
-    <View style={{ padding: 20 }}>
-      <Button title="Get API" onPress={getData} />
-      <Text>{data}</Text>
-    </View>
-  );
+  router.replace('/auth/Login')
 }
